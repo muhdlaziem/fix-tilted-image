ba6431828257a306139496787c16cc8eb7a94b3e
muhdlaziem <muhdlaziem@gmail.com>
2020-11-04 22:19:32 +0800

    a

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from skew_correction.skewer import Skewer
 # image show
 import cv2
 
-skewer = Skewer(image_url="sample.jpg")
+skewer = Skewer(image_url="https://raw.githubusercontent.com/muhdlaziem/fix-tilted-image/main/sample.jpg")
 
 rotated = skewer.get_rotated()
+original = cv2.imread("sample2.jpg")
 
 if skewer.is_rotated(): # Returns true or false according to any skew operation
     cv2.imshow("Rotated image", rotated)
+    cv2.imshow("Original image", original)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
